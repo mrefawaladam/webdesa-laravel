32aa69869cd83a20a12f0c4d9fbe607a485836ff
Muhammad Refa Walada Mushtofa <mrefawaladam@192.168.0.113>
2022-11-21 23:32:22 +0700

    make Penduduk Structural

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,24 @@ use App\Http\Controllers\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [HomeController::class,'index'])->name('home.index');
 
 Route::group(['middleware' => ['web', 'guest']], function () {
     Route::get('/masuk', [AuthController::class, 'index'])->name('masuk');
     Route::post('/masuk',[AuthController::class, 'masuk']);
 });
  
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::post('/keluar', [AuthController::class, 'keluar'])->name('keluar');
+    Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::get('/pengaturan', [UserController::class,'pengaturan'])->name('pengaturan');
+    Route::get('/profil', [UserController::class,'profil'])->name('profil');
+    Route::patch('/update-pengaturan/{user}', [UserController::class, 'updatePengaturan'])->name('update-pengaturan');
+    Route::patch('/update-profil/{user}', [UserController::class,'updateProfil'])->name('update-profil');
+
+
+    Route::get('/surat-harian',  [HomeController::class, 'suratHarian'])->name('surat-harian');
+    Route::get('/surat-bulanan',  [HomeController::class, 'suratBulanan'])->name('surat-bulanan');
+    Route::get('/surat-tahunan',  [HomeController::class, 'suratTahunan'])->name('surat-tahunan');
+    
+});
