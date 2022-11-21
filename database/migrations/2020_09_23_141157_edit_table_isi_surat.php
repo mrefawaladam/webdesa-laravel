9dd831bda8f343b6a7fe2ec7a9ae9b79933a70f0
berrylradianh <berrylhamesha@gmail.com>
2022-11-21 17:03:02 +0700

    isi surat migration

--- database/migrations/2020_09_23_141157_edit_table_isi_surat.php
+++ database/migrations/2020_09_23_141157_edit_table_isi_surat.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\IsiSurat;
+use App\IsiSurat;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
