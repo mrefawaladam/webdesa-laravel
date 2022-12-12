df4d8f506b1167655fd1351a9063e04f88febfa7
berrylradianh <berrylhamesha@gmail.com>
2022-12-12 15:30:03 +0700

    Kelola Berita Testing

--- cypress/e2e/kelola_berita.cy.js
+++ cypress/e2e/kelola_berita.cy.js
@@ -1,5 +1,126 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Kelola Berita', () => {
+  it('Admin can open Kelola Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
   })
+
+  it('Check Kembali button', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('a').contains('Kembali').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+  })
+
+  it('Admin can Tambah Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('input[name=gambar]').selectFile('cypress/fixtures/linkaja.png')
+    cy.get('input[name=judul]').type('Pemerintah Sudah Bisa Melakukan Pembayaran Menggunakan Link Aja')
+    cy.get('textarea[name=konten]').type('Kini Pembayaran, Pinjaman, & Investasi Online dalam satu genggaman. Cukup scan QR / kode tiket untuk pembayaran offline di seluruh merchant.', {force:true})
+    cy.get('button[id=simpan]').click()
+
+    cy.get('div').contains('Berita berhasil ditambahkan ')
+  })
+
+  it('Judul field is required when Tambah Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('input[name=gambar]').selectFile('cypress/fixtures/linkaja.png')
+    cy.get('textarea[name=konten]').type('Kini Pembayaran, Pinjaman, & Investasi Online dalam satu genggaman. Cukup scan QR / kode tiket untuk pembayaran offline di seluruh merchant.', {force:true})
+    cy.get('button[id=simpan]').click()
+
+    cy.get('span').contains('The judul field is required')
+  })
+
+  it('Konten field is required when Tambah Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('input[name=gambar]').selectFile('cypress/fixtures/linkaja.png')
+    cy.get('input[name=judul]').type('Pemerintah Sudah Bisa Melakukan Pembayaran Menggunakan Link Aja')
+    cy.get('button[id=simpan]').click()
+
+    cy.get('span').contains('The konten field is required')
+  })
+
+  it('Gambar field is required when Tambah Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('input[name=judul]').type('Pemerintah Sudah Bisa Melakukan Pembayaran Menggunakan Link Aja')
+    cy.get('textarea[name=konten]').type('Kini Pembayaran, Pinjaman, & Investasi Online dalam satu genggaman. Cukup scan QR / kode tiket untuk pembayaran offline di seluruh merchant.', {force:true})
+    cy.get('button[id=simpan]').click()
+
+    cy.get('span').contains('The gambar field is required')
+  })
+
+  it('All field are required when Tambah Berita', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Berita').click()
+    cy.url().should('contain', 'http://localhost:8000/kelola-berita')
+
+    cy.get('a').contains('Tambah Berita').click()
+    cy.get('button[id=simpan]').click()
+
+    cy.get('span').contains('The judul field is required')
+    cy.get('span').contains('The konten field is required')
+    cy.get('span').contains('The gambar field is required')
+  })
+
 })
