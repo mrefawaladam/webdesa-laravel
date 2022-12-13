6843b32a555bbbb52dfee2e0647310876b900e65
berrylradianh <berrylhamesha@gmail.com>
2022-12-13 08:16:31 +0700

    adding slider test

---
+++ cypress/e2e/kelola_slider.cy.js
@@ -0,0 +1,63 @@
+describe('Kelola Slider', () => {
+  it('Admin can open Kelola Slider', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Slider').click()
+    cy.url().should('contain', 'http://localhost:8000/slider')
+  })
+
+  it('Admin can Batal Tambah Gambar', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Slider').click()
+    cy.url().should('contain', 'http://localhost:8000/slider')
+
+    cy.get('a').contains('Tambah Gambar').click()
+    cy.get('.d-flex > .btn-white').click()
+  })
+
+  it('Admin can Tambah Gambar', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Slider').click()
+    cy.url().should('contain', 'http://localhost:8000/slider')
+
+    cy.get('a').contains('Tambah Gambar').click()
+    cy.get('input[name=gambar]').selectFile('cypress/fixtures/linkaja.png')
+    cy.get('textarea[name=caption]').type('Ini adalah caption')
+    cy.get('.d-flex > .btn-primary').click()
+
+    cy.get('div').contains('Gambar berhasil ditambahkan')
+  })
+
+  it('Admin can Hapus Gambar', () => {
+    cy.visit('http://localhost:8000/masuk')
+
+    cy.get('input[name=email]').type('admin@gmail.com')
+    cy.get('input[name=password]').type('password')
+    cy.get('button').contains('Masuk').click()
+    cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+    cy.get('a').contains('Kelola Slider').click()
+    cy.url().should('contain', 'http://localhost:8000/slider')
+
+    cy.get('.col-lg-4 > .mb-0').click()
+    cy.get('#form-hapus > .btn').click()
+    cy.get('div').contains('Gambar berhasil dihapus ')
+  })
+})
