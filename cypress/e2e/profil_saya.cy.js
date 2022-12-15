04a4b7e72aea946d3f4a52bf2dca3557d0a11a6b
berrylradianh <berrylhamesha@gmail.com>
2022-12-16 03:24:13 +0700

    adding profil sata test

---
+++ cypress/e2e/profil_saya.cy.js
@@ -0,0 +1,89 @@
+describe('Profil Saya', () => {
+    it('Admin can access Profil Saya', () => {
+        cy.visit('http://localhost:8000/masuk')
+
+        cy.get('input[name=email]').type('admin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Masuk').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.navbar-nav > .nav-item > .nav-link > .media > .avatar > img').click()
+
+        cy.get('.navbar-nav > .nav-item > .dropdown-menu > [href="http://localhost:8000/profil"]').click()
+        cy.url().should('contain', 'http://localhost:8000/profil')
+      })
+
+    //   cy.get('#input-nama')
+    //   cy.get('form > .btn')
+
+    it('Admin can edit Profil Saya', () => {
+        cy.visit('http://localhost:8000/masuk')
+
+        cy.get('input[name=email]').type('admin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Masuk').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.navbar-nav > .nav-item > .nav-link > .media > .avatar > img').click()
+
+        cy.get('.navbar-nav > .nav-item > .dropdown-menu > [href="http://localhost:8000/profil"]').click()
+        cy.url().should('contain', 'http://localhost:8000/profil')
+
+        cy.get('#input-nama').clear()
+        cy.get('#input-nama').type('Berryl Radian Hamesha')
+        cy.get('form > .btn').click()
+
+        cy.get('div').contains('Profil berhasil di perbarui')
+      })
+
+      it('Admin can Ganti Foto', () => {
+        cy.visit('http://localhost:8000/masuk')
+
+        cy.get('input[name=email]').type('admin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Masuk').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.navbar-nav > .nav-item > .nav-link > .media > .avatar > img').click()
+
+        cy.get('.navbar-nav > .nav-item > .dropdown-menu > [href="http://localhost:8000/profil"]').click()
+        cy.url().should('contain', 'http://localhost:8000/profil')
+
+        cy.get('#btn-ganti-foto_profil').selectFile('cypress/fixtures/linkaja.png')
+        cy.url().should('contain', 'http://localhost:8000/profil')
+      })
+
+      it('Admin can edit Email', () => {
+        cy.visit('http://localhost:8000/masuk')
+
+        cy.get('input[name=email]').type('admin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Masuk').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.navbar-nav > .nav-item > .nav-link > .media > .avatar > img').click()
+
+        cy.get('.navbar-nav > .nav-item > .dropdown-menu > [href="http://localhost:8000/profil"]').click()
+        cy.url().should('contain', 'http://localhost:8000/profil')
+
+        cy.get('.badge').click()
+        cy.url().should('contain', 'http://localhost:8000/pengaturan')
+      })
+
+      it('Admin can open Pengaturan', () => {
+        cy.visit('http://localhost:8000/masuk')
+
+        cy.get('input[name=email]').type('admin@gmail.com')
+        cy.get('input[name=password]').type('password')
+        cy.get('button').contains('Masuk').click()
+        cy.url().should('contain', 'http://localhost:8000/dashboard')
+
+        cy.get('.navbar-nav > .nav-item > .nav-link > .media > .avatar > img').click()
+
+        cy.get('.navbar-nav > .nav-item > .dropdown-menu > [href="http://localhost:8000/profil"]').click()
+        cy.url().should('contain', 'http://localhost:8000/profil')
+
+        cy.get('.col-4 > .btn').click()
+        cy.url().should('contain', 'http://localhost:8000/pengaturan')
+      })
+})
