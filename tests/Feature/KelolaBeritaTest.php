b9b010e3d51e0cbb5c3f5ed8a8f1b211b0e8988c
berrylradianh <berrylhamesha@gmail.com>
2022-12-15 17:28:27 +0700

    unit test

--- tests/Feature/KelolaBeritaTest.php
+++ tests/Feature/KelolaBeritaTest.php
@@ -17,102 +17,102 @@ class KelolaBeritaTest extends TestCase
      *
      * @return void
      */
-    public function test_admin_can_access_kelola_berita()
-    {
-        $credential = [
-            'email' => 'admin@gmail.com',
-            'password' => 'password'
-        ];
+    // public function test_admin_can_access_kelola_berita()
+    // {
+    //     $credential = [
+    //         'email' => 'admin@gmail.com',
+    //         'password' => 'password'
+    //     ];
 
-        $response = $this->post('masuk',$credential);
+    //     $response = $this->post('masuk',$credential);
 
-        $response = $this->get('http://localhost:8000/kelola-berita');
-        $response->assertStatus(200);
-    }
+    //     $response = $this->get('http://localhost:8000/kelola-berita');
+    //     $response->assertStatus(200);
+    // }
 
-    public function test_admin_can_tambah_kelola_berita()
-    {
-        Storage::fake('avatars');
+    // public function test_admin_can_tambah_kelola_berita()
+    // {
+    //     Storage::fake('avatars');
 
-        $response = $this->withoutMiddleware()->post('berita', [
-            'gambar' => UploadedFile::fake()->image('avatar.jpg'),
-            'judul' => 'Judul Testing',
-            'konten' => 'Konten Tes'
-        ]);
+    //     $response = $this->withoutMiddleware()->post('berita', [
+    //         'gambar' => UploadedFile::fake()->image('avatar.jpg'),
+    //         'judul' => 'Judul Testing',
+    //         'konten' => 'Konten Tes'
+    //     ]);
 
-        $response->assertValid(['Berita berhasil ditambahkan ']);
-        $response->assertStatus(302);
-    }
+    //     $response->assertValid(['Berita berhasil ditambahkan ']);
+    //     $response->assertStatus(302);
+    // }
 
-    public function test_judul_field_is_required_when_tambah_kelola_berita()
-    {
-        Storage::fake('avatars');
+    // public function test_judul_field_is_required_when_tambah_kelola_berita()
+    // {
+    //     Storage::fake('avatars');
 
-        $response = $this->withoutMiddleware()->post('berita', [
-            'gambar' => UploadedFile::fake()->image('avatar.jpg'),
-            'judul' => '',
-            'konten' => 'Konten Tes'
-        ]);
+    //     $response = $this->withoutMiddleware()->post('berita', [
+    //         'gambar' => UploadedFile::fake()->image('avatar.jpg'),
+    //         'judul' => '',
+    //         'konten' => 'Konten Tes'
+    //     ]);
 
-        $response->assertInvalid([
-            'judul' => 'The judul field is required.'
-        ]);
+    //     $response->assertInvalid([
+    //         'judul' => 'The judul field is required.'
+    //     ]);
 
-        $response->assertStatus(302);
-    }
+    //     $response->assertStatus(302);
+    // }
 
-    public function test_konten_field_is_required_when_tambah_kelola_berita()
-    {
-        Storage::fake('avatars');
+    // public function test_konten_field_is_required_when_tambah_kelola_berita()
+    // {
+    //     Storage::fake('avatars');
 
-        $response = $this->withoutMiddleware()->post('berita', [
-            'gambar' => UploadedFile::fake()->image('avatar.jpg'),
-            'judul' => 'Judul Test',
-            'konten' => ''
-        ]);
+    //     $response = $this->withoutMiddleware()->post('berita', [
+    //         'gambar' => UploadedFile::fake()->image('avatar.jpg'),
+    //         'judul' => 'Judul Test',
+    //         'konten' => ''
+    //     ]);
 
-        $response->assertInvalid([
-            'konten' => 'The konten field is required.'
-        ]);
+    //     $response->assertInvalid([
+    //         'konten' => 'The konten field is required.'
+    //     ]);
 
-        $response->assertStatus(302);
-    }
+    //     $response->assertStatus(302);
+    // }
 
-    public function test_gambar_field_is_required_when_tambah_kelola_berita()
-    {
-        Storage::fake('avatars');
+    // public function test_gambar_field_is_required_when_tambah_kelola_berita()
+    // {
+    //     Storage::fake('avatars');
 
-        $response = $this->withoutMiddleware()->post('berita', [
-            'gambar' => '',
-            'judul' => 'Judul Test',
-            'konten' => 'Konten Test'
-        ]);
+    //     $response = $this->withoutMiddleware()->post('berita', [
+    //         'gambar' => '',
+    //         'judul' => 'Judul Test',
+    //         'konten' => 'Konten Test'
+    //     ]);
 
-        $response->assertInvalid([
-            'gambar' => 'The gambar field is required.'
-        ]);
+    //     $response->assertInvalid([
+    //         'gambar' => 'The gambar field is required.'
+    //     ]);
 
-        $response->assertStatus(302);
-    }
+    //     $response->assertStatus(302);
+    // }
 
-    public function test_all_field_are_required_when_tambah_kelola_berita()
-    {
-        Storage::fake('avatars');
+    // public function test_all_field_are_required_when_tambah_kelola_berita()
+    // {
+    //     Storage::fake('avatars');
 
-        $response = $this->withoutMiddleware()->post('berita', [
-            'gambar' => '',
-            'judul' => '',
-            'konten' => ''
-        ]);
+    //     $response = $this->withoutMiddleware()->post('berita', [
+    //         'gambar' => '',
+    //         'judul' => '',
+    //         'konten' => ''
+    //     ]);
 
-        $response->assertInvalid([
-            'gambar' => 'The gambar field is required.',
-            'judul' => 'The judul field is required.',
-            'konten' => 'The konten field is required.'
-        ]);
+    //     $response->assertInvalid([
+    //         'gambar' => 'The gambar field is required.',
+    //         'judul' => 'The judul field is required.',
+    //         'konten' => 'The konten field is required.'
+    //     ]);
 
-        $response->assertStatus(302);
-    }
+    //     $response->assertStatus(302);
+    // }
 
     // public function test_admin_can_hapus_berita()
     // {
