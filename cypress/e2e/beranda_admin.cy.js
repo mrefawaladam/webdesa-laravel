0037129cc56d5dc51c7574e3f3e14176d97f398e
Muhammad Refa <aerefa55@gmail.com>
2022-12-16 09:03:53 +0700

    add testing Beranda

---
+++ cypress/e2e/beranda_admin.cy.js
@@ -0,0 +1,12 @@
+describe('Kelola Beranda', () => {
+    it('Admin can open Kelola Beranda Admin', () => {
+      cy.visit('http://localhost:8000/masuk')
+  
+      cy.get('input[name=email]').type('admin@gmail.com')
+      cy.get('input[name=password]').type('password')
+      cy.get('button').contains('Masuk').click()
+      cy.url().should('contain', 'http://localhost:8000') 
+    })
+   
+  })
+  
