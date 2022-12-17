3233681f92dfb40237f2d62238fe3686119b8ff6
vinkharisma <vinkharismadzania@gmail.com>
2022-12-17 14:02:15 +0700

    update keluar.cy

--- cypress/e2e/keluar.cy.js
+++ cypress/e2e/keluar.cy.js
@@ -1,5 +1,5 @@
 describe('Keluar', () => {
-  it('Admin can Logout', () => {
+  it('Admin can logout', () => {
       cy.visit('localhost:8000/masuk')
       cy.get('.mb-3 > .input-group > .form-control').type('admin@gmail.com')
       cy.get(':nth-child(3) > .input-group > .form-control').type('password')
